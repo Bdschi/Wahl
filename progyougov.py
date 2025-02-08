b0ef604860c09902ef11166e4744dae1ce818a71
Bernhard <kadmus@nexgo,de>
2025-02-08 16:41:32 +0100

    Split functions to module

--- progyougov.py
+++ progyougov.py
@@ -4,7 +4,7 @@ from itertools import combinations
 import prognosenfunc
 
 # Example usage:
-url = 'https://www.wahlrecht.de/umfragen/'  # Replace with the actual URL
+url = 'https://www.wahlrecht.de/umfragen/yougov.htm'  # Replace with the actual URL
 table_id = 'wilko'  # Replace with the actual table ID
 
 table_content = prognosenfunc.get_table_data(url, table_id)
@@ -13,16 +13,16 @@ table_content = prognosenfunc.get_table_data(url, table_id)
 for row in table_content:
     print(row)
 
-for i in list(range(0, len(table_content[0])-4))+[len(table_content[0])-3]:
-    print(f"{i}. {table_content[0][i+2]} ({table_content[1][i+2]})")
+for j in range(3, len(table_content)-2):
+    print(f"{j}. {table_content[j+2][0]}") 
     parties={}
-    for j in range(0, len(table_content)-4):
-        print(f"\t{j}. {table_content[j+2][0]} {table_content[j+2][i+2]}") 
+    for i in range(0, len(table_content[0])-5):
+        print(f"\t{i}. {table_content[0][i+2]} ({table_content[j+2][i+2]})")
         number = table_content[j+2][i+2].replace(",", ".").replace("%", "")
         number = number.replace('–','0')
         seats = float(number)*10
         if seats>=50:
-            parties[table_content[j+2][0]]=float(number)
+            parties[table_content[0][i+2]]=float(number)
     coalitions=prognosenfunc.find_coalitions(parties)
     for coalition in coalitions:
         print(f"\t {coalition}")
